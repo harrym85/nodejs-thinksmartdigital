ce1dd4a6fbd776df98d2c4dbe9bc48b7a96986a5
thembhani mapengo <harrymapengo@gmail.com>
2019-10-21 16:30:31 +0200

    node_refresher

--- play.js
+++ play.js
@@ -1,17 +1,58 @@
-const name = 'Thembhani '
-let age = 34
-const hashobbies = true
+// const name = 'Thembhani '
+// let age = 34
+// const hashobbies = true
 
 
-age = 30
-const summarizeUser = ( userName, userAge, userHasHobby) => {
-    return 'Name is ' + userName + ', age is ' + userAge + ' and the user has the hobbies: ' + userHasHobby
+// age = 30
+
+// const summarizeUser = ( userName, userAge, userHasHobby) => {
+//     return 'Name is ' 
+//     + userName + 
+//     ', age is ' + userAge +
+//      ' and the user has the hobbies: ' + 
+//      userHasHobby
+// }
+
+// const add = (a,b) => {
+//   return a + b
+// }
+
+// const assOne =(a) => a + 1
+
+
+// console.log(add(1,2))
+
+
+
+// const payment = (amount) => {
+//     return 'The total amount = '  + 'R ' +(amount + (0.15* amount))
+// }
+
+// console.log(payment(50))
+
+// console.log(summarizeUser(name,age,hashobbies))
+
+
+
+const person  = {
+   name: 'Harry',
+   age: 29,
+   greet() {
+     console.log('Hi, I am ' +  this.name)
+   }
 }
 
-const payment = (amount) => {
-    return 'The total amount = '  + 'R ' +(amount + (0.15* amount))
+person.greet();
+
+const hobbies = ['Asports','Cooking']
+
+for (let hobby of hobbies){
+    console.log(hobby)
 }
 
-console.log(payment(50))
+console.log(hobbies.map(hobby =>  'Hobby: ' + hobby));
+console.log(hobbies)
 
-console.log(summarizeUser(name,age,hashobbies))
+
+const copiedArray =  [...hobbies]
+console.log(copiedArray)
